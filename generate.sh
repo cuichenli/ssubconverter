0f1b681398e260eaba171c8e0f969166d31b54d4
Cuichen Li <cuichen.li94@gmail.com>
2025-01-23 21:36:22 +0800

    fix not uploading surfboard and pin gist id

--- generate.sh
+++ generate.sh
@@ -4,6 +4,7 @@ SUBCONVERTER_VERSION=${SUBCONVERTER_VERSION:-0.9.0}
 EXTRACTED_DIRECTORY=subconverter_release
 SUBSCRIPTION_URL=${SUBSCRIPTION_URL:?what are you doing? there is no SUBSCRIPTION_URL}
 GIST_TOKEN=${GIST_TOKEN:?what are you doing? there is no GIST_TOKEN}
+GIST_ID=${GIST_ID:?what are you doing? there is no GIST_ID}
 
 wget -O subconverter_release.tar.gz "https://github.com/tindy2013/subconverter/releases/download/v${SUBCONVERTER_VERSION}/subconverter_linux64.tar.gz"
 tar xvf ./subconverter_release.tar.gz 
@@ -18,17 +19,20 @@ path=output.yaml
 target=surfboard
 url=${SUBSCRIPTION_URL}
 upload=true
+upload_path=surfboard
 
 [clash]
 path=output.yaml
 target=clash
 url=${SUBSCRIPTION_URL}
 upload=true
+upload_path=clash
 EOF
 
 cat <<EOF > gistconf.ini
 [common]
 token = ${GIST_TOKEN}
+id = ${GIST_ID}
 EOF
 
-./subconverter -g
+./subconverter -g --artifact surfboard,clash --log out.tmp 
